f1b5e6d07e2e5233cdd6db19f15aacd5c9a98767
Kaspar <f.hannes1990@gmail.com>
2019-10-27 10:46:15 +0100

    quiz funktioniert

--- src/app/quiz-page/quiz-page.page.ts
+++ src/app/quiz-page/quiz-page.page.ts
@@ -9,13 +9,50 @@ import { Component, OnInit } from '@angular/core';
 export class QUIZPagePage implements OnInit {
   meinText: string;
 
+  richtig = 0;
+
+  insgesamt = 0;
+
   constructor(
     private vocabService: VocabService
 
   ) { }
 
   ngOnInit() {
-    this.meinText = this.vocabService.fragen[0].text;
+    console.log(this.vocabService.fragen)
+    
+    const index = this.getRandomInt(0,this.vocabService.fragen.length)
+    console.log(index)
+    console.log(this.vocabService.fragen[index])
+
+    this.meinText = this.vocabService.fragen[index].text;
+  
+  }
+  jaGeklickt() {
+    console.log("ja");
+    this.neueFrage();
+    this.insgesamt++;
+    this.richtig++;
+  }
+  
+  neinGeklickt () {
+    console.log("nein");
+    this.neueFrage();
+    this.insgesamt++;
   }
 
-}
+  neueFrage()  {
+    const index = this.getRandomInt(0,this.vocabService.fragen.length)
+    console.log(this.vocabService.fragen)
+    console.log(index)
+    console.log(this.vocabService.fragen[index])
+    this.meinText = this.vocabService.fragen[index].text;
+  }
+
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max -1);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  }
